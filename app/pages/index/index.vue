6e7312dd60dc1094a5ea2967d03ea961e98e6a04
Ethan S <iimsyw@gmail.com>
2023-02-20 12:39:40 +0800

    add app

---
+++ app/pages/index/index.vue
@@ -0,0 +1,41 @@
+<template>
+	<view class="top-box">
+		<view class="content">
+			<image src="../../static/hand-in-hand.svg" mode=""></image>
+			<text class="title">{{title}}</text>
+		</view>
+		<view class="login-button">
+			<button>Login</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Seren'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	.top-box {
+		background-color: #F43F5E;
+		
+		.login-button{
+			button{
+				background-color: #FACC15;
+				
+			}
+		}
+		
+	}
+</style>
