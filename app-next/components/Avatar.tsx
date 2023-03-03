f6f5b5866a507675722142146b7b3459b268a6e4
Ray Victor <ray7551@gmail.com>
2023-03-04 00:21:08 +0800

    avatar component

--- app-next/components/Avatar.tsx
+++ app-next/components/Avatar.tsx
@@ -2,12 +2,13 @@
 import React, { useState, useEffect, useRef } from 'react'
 import Image from 'next/image'
 import AvatarProps from '@/type/AvatarProps'
+import style from './styles/Avatar.module.css'
 
-export default function Avatar({ url, width, height }: AvatarProps) {
+export default function Avatar({ className, url, width, height }: AvatarProps) {
   return (
-    <div className='avatar'>
+    <div className={className + ' ' + style.avatar}>
       <Image
-        className='avatar-image'
+        className={style.avatarImage}
         src={url}
         alt="image"
         width={width ?? 50}
