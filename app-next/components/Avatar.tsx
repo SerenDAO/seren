ca0bad505390736ac3857d1738455d2804d331e5
Ray Victor <ray7551@gmail.com>
2023-03-04 09:40:05 +0800

    preventDefault

--- app-next/components/Avatar.tsx
+++ app-next/components/Avatar.tsx
@@ -3,16 +3,39 @@ import React, { useState, useEffect, useRef } from 'react'
 import Image from 'next/image'
 import AvatarProps from '@/type/AvatarProps'
 import style from './styles/Avatar.module.css'
+import { listeners } from 'process'
 
 export default function Avatar({ className, url, width, height }: AvatarProps) {
+  const ref = React.createRef<HTMLImageElement>()
+
+  // prevent default behavior of image when touched
+  useEffect(() => {
+    const image = ref.current
+    const listener = (e: TouchEvent) => {
+      e.preventDefault()
+    }
+    if (image) {
+      image.addEventListener('touchstart', listener)
+    }
+    return () => {
+      if (image) {
+        image.removeEventListener('touchstart', listener)
+      }
+    }
+  }, [ref])
+
   return (
     <div className={(className || '') + ' ' + style.avatar}>
       <Image
+        ref={ref}
         className={style.avatarImage}
         src={url}
         alt="image"
         width={width ?? 50}
         height={height ?? 50}
+        onTouchStart={(e) => {
+          e.preventDefault()
+        }}
       />
     </div>
   )
